7b174f79f7b9ac9bb9d38bd20aa9292b599ec66e
dekk <dekkservidores@gmail.com>
2018-05-18 23:11:39 -0500

    Usuario y token listo

--- app.js
+++ app.js
@@ -2,25 +2,29 @@
 
 var express = require("express");
 var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
 
 var app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+var appRoutes = require("./src/route/app");
+var usuarioRoutes = require("./src/route/usuario.route");
+var login = require("./src/route/login");
+
 mongoose.connection.openUri('mongodb://localhost/hospitalDB', (err, res) =>{
-
-if (err) {
-    throw err;
-}else {
-    console.log("Base de datos: \x1b[32m%s\x1b[0m", "online"); 
-}
-
+    if (err) {
+        throw err;
+    }else {
+        console.log("Base de datos: \x1b[32m%s\x1b[0m", "online"); 
+    }
 });
 
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Petición realizada correctamente'
-    })
-})
+app.use('/usuario', usuarioRoutes);
+app.use('/login', login);
+app.use('/', appRoutes);
 
 app.listen(3000, () => {
     console.log("Express server puerto 3000: \x1b[32m%s\x1b[0m", "online");
